f1ca9e6ae6382f517b0eab19129c06a3e0d831bc
William Ye <yehaojing@protonmail.com>
2022-08-06 17:27:39 +1000

    part 3.3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -36,6 +36,19 @@ app.get('/info', (request, response) => {
         <div>${new Date()}</div>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const person = persons.find(person => {
+        return person.id === id
+    })
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
