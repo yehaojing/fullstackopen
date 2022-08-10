c39683ace6346675105b27928242daf817670eb9
William Ye <yehaojing@protonmail.com>
2022-08-11 00:51:33 +1000

    part 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -5,8 +5,14 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.map(blog => blog.likes).reduce((likes1, likes2) => likes1 + likes2)
 }
+
+const favouriteBlog = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+    return blogs[likes.indexOf(Math.max(...likes))]
+}
     
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
