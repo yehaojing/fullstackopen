1982a3d88147fa41a71524396a2babde3d43f66d
William Ye <yehaojing@protonmail.com>
2022-08-17 08:32:52 +1000

    part 4.15

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blog')
+const userRouter = require('./controllers/user')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
@@ -11,5 +12,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 module.exports = app
--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
     title: String,
-    author: String,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
     url: String,
     likes: Number
 })
