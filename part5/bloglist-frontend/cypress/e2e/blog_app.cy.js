21bd92dd3444861e300374f7b1ac50a55752fd17
William Ye <yehaojing@protonmail.com>
2022-09-29 00:14:17 +1000

    part 5.21

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -20,7 +20,7 @@ describe('Blog app', function() {
   describe('Login', function () {
     it('login button can be clicked with empty form resulting in wrong username or password notification', function() {
       cy.contains('login').click()
-      cy.contains('Wrong username or password')
+      cy.get('.error').contains('Wrong username or password')
     })
 
     it('user can login', function() {
@@ -37,27 +37,46 @@ describe('Blog app', function() {
       cy.get('input:last').type(user.password)
       cy.contains('login').click()
       cy.contains(`Logged in as ${user.name}`)
-    })
 
-    it('A blog can be created', function () {
       cy.contains('new blog').click()
       cy.get(`[aria-label="Title"]`).type('Test Blog')
       cy.get(`[aria-label="Author"]`).type('Test Author')
       cy.get(`[aria-label="URL"]`).type('testurl.com')
       cy.contains('Create').click()
+    })
+
+    it('A blog can be created', function () {
       cy.contains('Test Blog Test Author')
     })
 
     it('A blog can be liked', function () {
-      cy.contains('new blog').click()
-      cy.get(`[aria-label="Title"]`).type('Test Blog')
-      cy.get(`[aria-label="Author"]`).type('Test Author')
-      cy.get(`[aria-label="URL"]`).type('testurl.com')
-      cy.contains('Create').click()
       cy.contains('show').click()
       cy.contains('Likes: 0')
       cy.contains('Like').click()
       cy.contains('Likes: 1')
+    })
+
+    it('A blog can be deleted', function () {
+      cy.contains('show').click()
+      cy.contains('remove').click()
+      cy.contains('show').should('not.exist')
+    })
+
+    it('If not creator of blog, blog cannot be deleted', function () {
+      const secondaryUser = {
+        username: 'cypress-test-user2',
+        name: 'Secondary Cypress Test User',
+        password: 'password1234'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', secondaryUser)
+      cy.contains('logout').click()
+      cy.get('input:first').type(secondaryUser.username)
+      cy.get('input:last').type(secondaryUser.password)
+      cy.contains('login').click()
+      cy.contains(`Logged in as ${secondaryUser.name}`)
+      cy.contains('show').click()
+      cy.contains('remove').click()
+      cy.get('.error').contains('AxiosError: Request failed with status code 401')
     })
   })
 
