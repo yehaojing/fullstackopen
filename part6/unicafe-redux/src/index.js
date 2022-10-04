398575ea0007bf78cc93fcea3da8230442a00c93
William Ye <yehaojing@protonmail.com>
2022-10-04 23:56:09 +1100

    part 6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -6,21 +6,20 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
+
+  const actionDispatcher = (action) => {
+    return () => { store.dispatch({ type: action }) }
   }
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={actionDispatcher('GOOD')}>good</button>
+      <button onClick={actionDispatcher('OK')}>ok</button>
+      <button onClick={actionDispatcher('BAD')}>bad</button>
+      <button onClick={actionDispatcher('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
