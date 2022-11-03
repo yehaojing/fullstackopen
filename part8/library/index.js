325ce6805940bbca7b8e9be76b082fc9349634bb
William Ye <yehaojing@protonmail.com>
2022-11-04 09:00:35 +1100

    part 8.7

--- part8/library/index.js
+++ part8/library/index.js
@@ -123,6 +123,11 @@ const typeDefs = gql`
       author: String!
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -156,6 +161,15 @@ const resolvers = {
       books = books.concat(book)
 
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if (author) {
+        author.born = args.setBornTo
+        return author
+      } else {
+        return null
+      }
     }
   }
 }
