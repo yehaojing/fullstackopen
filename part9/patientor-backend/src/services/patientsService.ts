87894e2334dbd4c6bfd6497b1e53769ec42cb72d
William Ye <yehaojing@protonmail.com>
2022-11-24 08:36:33 +1100

    part 9.19

--- part9/patientor-backend/src/services/patientsService.ts
+++ part9/patientor-backend/src/services/patientsService.ts
@@ -1,9 +1,9 @@
 import { v4 as uuid } from 'uuid';
-import patientsData from '../data/patients.json';
+import patientsData from '../data/patients';
 
 import { SensitivePatient, NewPatient, Patient } from '../types';
 
-const patients: Array<Patient> = patientsData as Array<Patient>;
+const patients: Array<Patient> = patientsData;
 
 const getEntries = (): Array<SensitivePatient> => {
   return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({
--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,30 +1,72 @@
 export interface Diagnose {
-    code: string,
-    name: string,
-    latin?: string;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+  code: string;
+  name: string;
+  latin?: string;
 }
 
 export interface Patient {
-    id: string,
-    name: string,
-    dateOfBirth: string,
-    ssn: string,
-    gender: string,
-    occupation: string,
-    entries: Entry[]
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
+  entries: Entry[];
 }
 
-export type SensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+export type SensitivePatient = Omit<Patient, "ssn" | "entries">;
 
-export type NewPatient = Omit<Patient, 'id'>;
+export type NewPatient = Omit<Patient, "id">;
 
 export enum Gender {
-    Male = 'male',
-    Female = 'female',
-    Other = 'other'
-  }
-  
+  Male = "male",
+  Female = "female",
+  Other = "other",
+}
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export interface sickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare"
+  employerName: string;
+  sickLeave?: sickLeave
+}
+
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
