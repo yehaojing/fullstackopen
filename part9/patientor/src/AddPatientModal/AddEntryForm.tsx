4e1aa9cada548c92c1c2233db87e6c303eb4b2b8
William Ye <yehaojing@protonmail.com>
2022-12-10 23:11:33 +1100

    part 9.27

--- part9/patientor/src/AddPatientModal/AddEntryForm.tsx
+++ part9/patientor/src/AddPatientModal/AddEntryForm.tsx
@@ -5,11 +5,12 @@ import { ToggleButtonGroup, ToggleButton } from "@mui/material";
 import { Field, Formik, Form } from "formik";
 
 import { DiagnosisSelection, HealthCheckOption, SelectField, TextField } from "./FormField";
-import { Entry, HealthCheckEntry, HealthCheckRating, HospitalEntry } from "../types";
+import { Entry, HealthCheckEntry, HealthCheckRating, HospitalEntry, OccupationalHealthcareEntry } from "../types";
 
 export type EntryFormValues = Entry;
 export type HospitalEntryFormValues = HospitalEntry;
 export type HealthCheckEntryFormValues = HealthCheckEntry;
+export type OccupationalHealthCareEntryFormValues = OccupationalHealthcareEntry;
 
 interface EntryProps {
   onSubmit: (values: EntryFormValues) => void;
@@ -25,6 +26,12 @@ interface HospitalEntryProps {
 
 interface HealthCheckEntryProps {
   onSubmit: (values: HealthCheckEntryFormValues) => void;
+  onCancel: () => void;
+  patientId: string;
+}
+
+interface OccupationalHealthCareEntryProps {
+  onSubmit: (values: OccupationalHealthCareEntryFormValues) => void;
   onCancel: () => void;
   patientId: string;
 }
@@ -53,6 +60,7 @@ export const AddEntryForm = ({ onSubmit, onCancel, patientId }: EntryProps) => {
       <ToggleButtonGroup value={entryType} exclusive onChange={handleChange}>
         <ToggleButton value="Hospital">Hospital</ToggleButton>
         <ToggleButton value="HealthCheck">Healh Check</ToggleButton>
+        <ToggleButton value="OccupationalHealthcare">Occupational Healthcare</ToggleButton>
       </ToggleButtonGroup>
       <Divider style={{ margin: 5 }}></Divider>
       {entryType === "Hospital" &&
@@ -62,6 +70,11 @@ export const AddEntryForm = ({ onSubmit, onCancel, patientId }: EntryProps) => {
         patientId={patientId}
       />}
       {entryType === "HealthCheck" && <HealthCheckForm
+        onSubmit={onSubmit}
+        onCancel={onCancel}
+        patientId={patientId}
+      />}
+      {entryType === "OccupationalHealthcare" && <OccupationalHealthCareEntryForm
         onSubmit={onSubmit}
         onCancel={onCancel}
         patientId={patientId}
@@ -105,6 +118,9 @@ const HospitalEntryForm = ({ onSubmit, onCancel, patientId }: HospitalEntryProps
         }
         if (!Date.parse(values.discharge.date)) {
           errors.discharge = { date: dateError };
+        }
+        if (!values.discharge.criteria) {
+          errors.discharge = { criteria: requiredError };
         }
         return errors;
       }}
@@ -245,6 +261,128 @@ const HealthCheckForm = ({ onSubmit, onCancel, patientId }: HealthCheckEntryProp
               label="Health Check Rating"
               name="healthCheckRating"
               options={ratingOptions}
+            />
+            <Grid>
+              <Grid item>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  style={{ float: "left" }}
+                  type="button"
+                  onClick={onCancel}
+                >
+                  Cancel
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  style={{
+                    float: "right",
+                  }}
+                  type="submit"
+                  variant="contained"
+                  disabled={!dirty || !isValid}
+                >
+                  Add
+                </Button>
+              </Grid>
+            </Grid>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+const OccupationalHealthCareEntryForm = ({ onSubmit, onCancel, patientId }: OccupationalHealthCareEntryProps) => {
+  const [{ diagnoses }] = useStateValue();
+  return (
+    <Formik
+      initialValues={{
+        id: patientId,
+        type: "OccupationalHealthcare",
+        description: "",
+        date: "",
+        specialist: "",
+        employerName: "",
+        sickLeave: {
+          startDate: "",
+          endDate: "",
+        },
+        diagnosisCodes: [],
+      }}
+      onSubmit={onSubmit}
+      validate={(values) => {
+        const requiredError = "Field is required";
+        const dateError = "Date is incorrectly formatted";
+        const errors: { [field: string]: any } = { };
+        if (!values.description) {
+          errors.description = requiredError;
+        }
+        if (!values.date) {
+          errors.date = requiredError;
+        }
+        if (!values.specialist) {
+          errors.specialist = requiredError;
+        }
+        if (!Date.parse(values.date)) {
+          errors.date = dateError;
+        }
+        if (!values.employerName) {
+          errors.employerName = requiredError;
+        }
+        if (values.sickLeave && !Date.parse(values.sickLeave.startDate)) {
+          errors.sickLeave = { startDate: dateError };
+        }
+        if (values.sickLeave && !Date.parse(values.sickLeave.endDate)) {
+          errors.sickLeave = { endDate: dateError };
+        }
+        return errors;
+      }}
+    >
+      {({ isValid, dirty, setFieldValue, setFieldTouched }) => {
+        return (
+          <Form className="form ui">
+            <Field
+              label="Description"
+              placeholder="Description"
+              name="description"
+              component={TextField}
+            />
+            <Field
+              label="Date"
+              placeholder="YYYY-MM-DD"
+              name="date"
+              component={TextField}
+            />
+            <Field
+              label="Specialist"
+              placeholder="Specialist"
+              name="specialist"
+              component={TextField}
+            />
+            <DiagnosisSelection
+              setFieldValue={setFieldValue}
+              setFieldTouched={setFieldTouched}
+              diagnoses={Object.values(diagnoses)}
+            />
+            <Field
+              label="Employer Name"
+              placeholder="Employer Name"
+              name="employerName"
+              component={TextField}
+            />
+            <Field
+              label="Sick Leave Start Date"
+              placeholder="YYYY-MM-DD"
+              name="sickLeave.startDate"
+              component={TextField}
+            />
+            <Field
+              label="Sick Leave End Date"
+              placeholder="YYYY-MM-DD"
+              name="sickLeave.endDate"
+              component={TextField}
             />
             <Grid>
               <Grid item>
