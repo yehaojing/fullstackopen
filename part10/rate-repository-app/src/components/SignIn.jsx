de09ee14121d0c1a37ffa9039b42c60b590a8920
William Ye <yehaojing@protonmail.com>
2022-12-12 00:00:41 +1100

    part 10.8

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,62 @@
-import Text from './Text';
+import { View, StyleSheet, Button } from "react-native";
+import FormikTextInput from "./FormikTextInput";
+import { Formik } from 'formik';
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  signInContainer: {
+    padding: 5,
+    backgroundColor: theme.colors.appBarPrimary
+  },
+  textInputs: {
+    marginVertical: 7,
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: theme.colors.textSecondary
+  },
+  loginButton: {
+    marginVertical: 7,
+    paddingVertical: 7,
+    borderRadius: 5,
+    color: theme.colors.appBarPrimary,
+    backgroundColor: theme.colors.primary,
+    alignSelf: "center",
+    flex: "auto"
+  },
+});
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View style={styles.signInContainer}>
+      <FormikTextInput
+        name="username"
+        placeholder="Username"
+        style={styles.textInputs}
+        placeholderTextColor={theme.colors.textSecondary}
+      />
+      <FormikTextInput
+        name="password"
+        placeholder="Password"
+        style={styles.textInputs}
+        secureTextEntry
+        placeholderTextColor={theme.colors.textSecondary}
+      />
+      <Button style={styles.loginButton} onPress={onSubmit} title="Login"/>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
-};
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <Formik initialValues={{username: '', password: ''}} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
+}
 
 export default SignIn;
