bb9e897f7b10af0308dc73801c048d3fe7d3698e
William Ye <yehaojing@protonmail.com>
2022-12-12 01:50:51 +1100

    part 10.10

--- part10/rate-repository-app/src/theme.js
+++ part10/rate-repository-app/src/theme.js
@@ -1,3 +1,11 @@
+import { Platform } from "react-native";
+
+const font = Platform.select({
+  android: "Roboto",
+  ios: "Arial",
+  default: "System",
+});
+
 const theme = {
   colors: {
     textPrimary: "#24292e",
@@ -5,14 +13,14 @@ const theme = {
     primary: "#0366d6",
     appBarPrimary: "#ffffff",
     background: "#c4c4c4",
-    error: "#8b0000"
+    error: "#8b0000",
   },
   fontSizes: {
     body: 14,
     subheading: 16,
   },
   fonts: {
-    main: "System",
+    main: font,
   },
   fontWeights: {
     normal: "400",
