4165103121fa38bdd9d83b18ee17b8969da4d516
William Ye <yehaojing@protonmail.com>
2023-01-18 20:46:42 +1100

    part 11.6

--- part11/full-stack-open-pokedex/.eslintrc.js
+++ part11/full-stack-open-pokedex/.eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   "env": {
+    "commonjs": true,
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
