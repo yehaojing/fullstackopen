9d1348f9ec3b0f428dd086c58c2ad2208c867780
William Ye <yehaojing@protonmail.com>
2023-02-02 23:57:54 +1100

    part 13.4

--- part13/notes/index.js
+++ part13/notes/index.js
@@ -13,33 +13,59 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   // commented out above because I'm running PostgreSQL in a container
 });
 
-class Note extends Model {}
-Note.init(
+class Blog extends Model {}
+Blog.init(
   {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    content: { type: DataTypes.TEXT, allowNull: false },
-    important: { type: DataTypes.BOOLEAN },
-    date: { type: DataTypes.DATE },
+    author: { type: DataTypes.TEXT },
+    url: { type: DataTypes.TEXT, allowNull: false },
+    title: { type: DataTypes.TEXT, allowNull: false },
+    likes: { type: DataTypes.INTEGER, defaultValue: 0 },
   },
-  { sequelize, underscored: true, timestamps: false, modelName: "note" }
+  { sequelize, underscored: true, timestamps: false, modelName: "blog" }
 );
+Blog.sync();
 
-app.get("/api/notes", async (req, res) => {
+app.get("/api/blogs", async (req, res) => {
   try {
-    const notes = await Note.findAll();
-    res.json(notes);
+    const blogs = await Blog.findAll();
+    console.log(JSON.stringify(blogs, null, 2));
+    res.json(blogs);
   } catch (error) {
     return res.status(400).json({ error });
   }
 });
 
-app.post("/api/notes", async (req, res) => {
+app.get("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    console.log(blog.toJSON());
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.use(express.json());
+
+app.post("/api/blogs", async (req, res) => {
+  console.log(req);
   try {
     console.log(req.body);
-    const note = await Note.create(req.body);
-    res.json(note);
+    const blog = await Blog.create(req.body);
+    res.json(blog);
   } catch (error) {
     return res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.destroy();
+    res.json(blog);
+  } else {
+    res.status(404).end();
   }
 });
 
