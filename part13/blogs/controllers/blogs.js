0ee69025f459bcefecd3c8ea542bcbc54564c8b7
William Ye <yehaojing@protonmail.com>
2023-02-04 16:08:22 +1100

    part 13.14

--- part13/blogs/controllers/blogs.js
+++ part13/blogs/controllers/blogs.js
@@ -7,11 +7,14 @@ const { Op } = require("sequelize");
 router.get("/", async (req, res) => {
   const where = {};
 
-  if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`,
-    };
+  const search = req.query.search
+  if (search) {
+    where[Op.or] = [
+      { title: { [Op.iLike]: `%${search}%` } },
+      { author: { [Op.iLike]: `%${search}%` } },
+    ];
   }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ["userId"] },
     include: {
