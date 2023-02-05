7cd0ec79db57bdd657e743325882f643ca423e84
William Ye <yehaojing@protonmail.com>
2023-02-06 01:25:38 +1100

    part 13.22

--- part13/blogs/controllers/readingLists.js
+++ part13/blogs/controllers/readingLists.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const { ReadingList } = require("../models");
+const tokenExtractor = require("../middleware/tokenExtractor");
 
 router.post("/", async (req, res) => {
   const { blogId, userId } = req.body;
@@ -11,6 +12,32 @@ router.post("/", async (req, res) => {
   });
 
   res.json(readingListEntry);
+});
+
+router.put("/:id", tokenExtractor, async (req, res) => {
+  // you can also treat :id as the id for the blog entry
+  const userId = req.decodedToken.id;
+  const readingListEntryId = req.params.id;
+
+  const readingListEntry = await ReadingList.findByPk(readingListEntryId);
+
+  if (readingListEntry) {
+    if (readingListEntry.userId === userId) {
+      readingListEntry.read = true;
+      readingListEntry.save();
+      res.json({ read: true });
+    } else {
+      const err = new Error(`Unauthorised marking of reading list entry.`);
+      err.name = "Unauthorised";
+      throw err;
+    }
+  } else {
+    const err = new Error(
+      `Reading list entry with id '${readingListEntryId}' not found.`
+    );
+    err.name = "BlogNotFound";
+    throw err;
+  }
 });
 
 module.exports = router;
