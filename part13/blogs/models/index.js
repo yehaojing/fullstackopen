6daf80dca7d695252699c4f398ce02a9b2e1d837
William Ye <yehaojing@protonmail.com>
2023-02-06 00:31:10 +1100

    part 13.19

--- part13/blogs/models/index.js
+++ part13/blogs/models/index.js
@@ -1,10 +1,15 @@
 const Blog = require("./blog");
 const User = require("./user");
+const ReadingList = require("./readingList");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
+User.belongsToMany(Blog, { through: ReadingList });
+Blog.belongsToMany(User, { through: ReadingList });
+
 module.exports = {
   Blog,
   User,
+  ReadingList,
 };
--- part13/blogs/utils/db.js
+++ part13/blogs/utils/db.js
@@ -4,19 +4,6 @@ const { Umzug, SequelizeStorage } = require("umzug");
 
 const sequelize = new Sequelize(DATABASE_URL);
 
-const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: { glob: "migrations/*.js" },
-    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  });
-  const migrations = await migrator.up();
-  console.log("Migrations up to date", {
-    files: migrations.map((mig) => mig.name),
-  });
-};
-
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
@@ -31,4 +18,24 @@ const connectToDatabase = async () => {
   return null;
 };
 
-module.exports = { connectToDatabase, sequelize };
+const migrationConf = {
+  migrations: { glob: "migrations/*.js" },
+  storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
+};
+module.exports = { connectToDatabase, sequelize, rollbackMigration };
